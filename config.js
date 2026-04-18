2941735b252b60e25de6c5935c5e05ff8e3e916e
ajohns <jjohnson.arun@gmail.com>
2026-04-18 15:23:21 +0530

    Remove <filename> from tracking

--- config.js
+++
@@ -1,8 +0,0 @@
-const APP_CONFIG = {
-  GOOGLE_CLIENT_NAME: "202116198204-m2d1r7ms91109aev834ue5iefp33923n.apps.googleusercontent.com",
-  LAMBDA_URLS: {
-    GET_ATTENDANCE: "LAMBDA_GET_ATTENDANCE",
-    MARK_ATTENDANCE: "LAMBDA_MARK_ATTENDANCE",
-    LOGIN: "https://lgcmvylgr6keesaazyb3yq6o6i0ayztl.lambda-url.ap-south-1.on.aws/",
-  }
-};
